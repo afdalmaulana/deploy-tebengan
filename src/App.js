5ebfa97e86f807bc84f5899f929ff4fac404923f
Muh. Afdal Maulana Said <afdalmaulanaaa@gmail.com>
2023-12-02 01:34:25 +0800

    testing npm i

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Testing Tebengan
+          Testing Tebengan2
         </a>
       </header>
     </div>
