e48e3abfa3c8d26acbe234afec256f9584ec4d27
Muh. Afdal Maulana Said <afdalmaulanaaa@gmail.com>
2023-12-02 01:27:53 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing Tebengan
         </a>
       </header>
     </div>
